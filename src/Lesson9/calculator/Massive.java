bf31585df9cbccda972c445c8596d442c5dd3733
Gurvin576 <131300490+Gurvin576@users.noreply.github.com>
2023-06-09 23:20:02 +0300

    ,kf,kf,kd

---
+++ src/Lesson9/calculator/Massive.java
@@ -0,0 +1,11 @@
+package Lesson9.calculator;
+
+public class Massive extends CycleSwitchCase{
+    double massResult[]=new double[10];
+    private double Results() {
+        for (int i = 0; i < massResult.length; i++) {
+        }
+        return 0;
+    }
+
+}

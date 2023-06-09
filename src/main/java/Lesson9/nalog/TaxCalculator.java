96d8b2a0290bae9726b5209ea3a1d35fdd5581f2
Волобуев Александр <aleksandr.volobuev@gazprombank.ru>
2023-06-10 02:47:10 +0300

    Задача про расчет налога. Начало. Добавлены тесты по TDD

---
+++ src/main/java/Lesson9/nalog/TaxCalculator.java
@@ -0,0 +1,18 @@
+package Lesson9.nalog;
+
+public class TaxCalculator {
+    public static void main(String[] args) {
+        // TODO если нужно, реализовать поставку данных (через консоль или файл или ..etc)
+    }
+
+    /**
+     * Метод, расчитывающий налог
+     * //TODO расписать коротко как считает
+     * @param prices - входной массив ценников
+     * @return - посчитанный налог
+     */
+    public static Double calculateTax(String[] prices) {
+        //TODO implement
+        return null;
+    }
+}
